4185f835c2808510c0f2f500127469356b58ee04
rafaringa <rafaelfm@ufba.br>
2021-12-21 16:50:54 -0300

    final

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,21 @@
-var todolist = document.getElementById('todolist');
+var listaTarefas = document.getElementById('tarefaspendentes');
+var idTarefa = 0;
 
 function insereTarefa(){
-    let valor = document.getElementsByTagName('input')[0].value;
-    let htmltarefa = '<span><input type="checkbox" />'+ valor + '</span>';
-    todolist.insertAdjacentHTML("afterend", htmltarefa);
+    let tarefadigitada = document.getElementById('inputnovatarefa');
+    let htmltarefa = '<span class="w-100 py-2 mb-2 btn btn-outline-dark rounded-4 text-left" type="submit" id="t'+ idTarefa +'" onClick="tarefaConcluida(this.id)"><p>Feito</p>'+ tarefadigitada.value + '</span>';
+    listaTarefas.insertAdjacentHTML("afterend", htmltarefa);
+    tarefadigitada.value = '';
+    idTarefa++;
+}
+
+function tarefaConcluida(pegaId){
+    let spanTarefa = document.getElementById(pegaId);
+    if(!spanTarefa.classList.contains('tarefaConcluida')){
+        spanTarefa.classList.add("tarefaConcluida");
+        console.log("Done!");
+    } else {
+        spanTarefa.classList.remove("tarefaConcluida");
+        console.log('Desdone');
+    }
 }
